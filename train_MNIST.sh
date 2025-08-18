fb78fee1f94de459219e6d7f62fa8ee7d7d37c7c
clarkmiyamoto <cm6627@nyu.edu>
2025-08-18 14:34:08 -0400

    acp

---
+++ train_MNIST.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=train
+#SBATCH --nodes=1
+#SBATCH -p GPU
+#SBATCH -t 2:00:00
+#SBATCH --gpus=v100-16:8 
+
+set -x  # Echo commands to stdout
+
+module activate AI/pytorch_23.02-1.13.1-py3 # Activate PyTorch
+
+cd /jet/home/cmiyamot/upscaling-ddpm
+
+
+# Run code
+python train_MNIST.py
