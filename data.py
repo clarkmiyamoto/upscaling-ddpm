1cf4582f44224ea81d218838f79110e6197a1c2e
clarkmiyamoto <cmiyamot@usc.edu>
2025-08-18 14:39:46 -0400

    lazy commit

--- data.py
+++ data.py
@@ -11,3 +11,14 @@ def get_MNIST(batch_size: int) -> DataLoader:
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
     
     return dataloader
+
+def get_FashionMNIST(batch_size: int) -> DataLoader:
+    transform = tv.transforms.Compose([
+        tv.transforms.ToTensor(),
+        tv.transforms.Normalize(0.5, 0.5),  # [-1,1] for all channels if grayscale; for RGB use per-channel tuples
+    ])
+
+    dataset = tv.datasets.FashionMNIST(root="./data", train=True, download=True, transform=transform)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    
+    return dataloader
